e9294a5b2caddefbb344190e8ed31e216e3e6e4c
kevinfrancomme <kevin_francomme@hotmail.fr>
2019-09-17 16:32:56 +0200

    Update package.json

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,5 +1,5 @@
 self.addEventListener('push', function(event) {
-    let msg = event.data.json()
+    let msg = event.data.json();
     const promiseChain = self.registration.showNotification(msg.title, msg.options);
 
     event.waitUntil(promiseChain);
