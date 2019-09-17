e4513b8f32ed329f6dcd8905208f15b627bd5693
kevinfrancomme <kevin_francomme@hotmail.fr>
2019-09-17 16:19:13 +0200

    Init project

---
+++ NOTversioned/vapidKeys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicKey: "BP5PvZXKSWWBmrJZ1rSc-_uVDmIRK1NpakGDvCbkuHE_adkSl_Hz4bImV7RuwIsT6gswyvAfq89s4epEh6trsos",
+    privateKey: "WqUxK1pr6cbf16Tp4nym3-EXRpHBqtrww_2MsFO40Kk"
+};
