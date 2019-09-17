cedcaf44f1366364720e75e5702002d2165ae84b
kevinfrancomme <kevin_francomme@hotmail.fr>
2019-09-17 16:37:18 +0200

    Update port

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.post('/api/trigger-push-msg', (req,res) => {
 
 app.use(express.static('public'));
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 80, function () {
     console.log('Push server start!')
 });
 
